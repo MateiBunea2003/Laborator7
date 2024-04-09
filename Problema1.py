c9b84a8ef99aea41260b75ddfe8ab531fa562ec0
MateiBun2003 <matei.bunea@ulbsibiu.ro>
2024-04-09 10:22:44 +0300

    Problema1 -pyton

---
+++ Problema1.py
@@ -0,0 +1,42 @@
+class Calculator:
+    def __init__(self, value):
+        self.value = value
+
+    def add(self, ather):
+        self.value += ather
+        return self
+
+    def multiply(self, ather):
+        self.value *= ather
+        return self
+
+    def divide(self, ather):
+        self.value /= ather
+        return self
+
+    def get_result(self):
+        return self.value
+
+
+class Calculator2(Calculator):
+    def __init__(self, value):
+        super().__init__(value)
+
+    def sqrt(self):
+        self.value = self.value ** 0.5
+
+
+if __name__ == "__main__":
+    c = Calculator(5)
+    c.add(3).multiply(2).divide(4)
+    print(c.get_result())
+
+    print("------------------------------")
+
+    print(
+        Calculator(5)
+        .add(3)
+        .multiply(2)
+        .divide(4)
+        .get_result()
+    )
